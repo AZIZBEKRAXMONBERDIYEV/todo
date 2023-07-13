4a245c5f21c3eda4ad6e33a34f89ac89435c3276
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-13 17:24:10 +0500

    list filter

--- api/admin.py
+++ api/admin.py
@@ -3,13 +3,9 @@ from django.contrib import admin
 from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed', 'user_id', 'more_info')
+    list_display = ('title',)
+    list_filter = ('completed', 'created', 'updated')
 
-    def user_id(self, obj):
-        return obj.user.id
-
-    def more_info(self, obj):
-        return f"{obj.title} - {obj.description} - {obj.completed} - {obj.user.username}"
 
 
 admin.site.register(Task, TaskAdmin)
