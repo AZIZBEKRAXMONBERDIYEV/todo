761b8759c90823fc918fc2bf463c1644ec3091b7
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-13 17:50:40 +0500

    actions

--- api/admin.py
+++ api/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 
 from .models import Task
 
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'completed')
     list_filter = ('completed', 'created', 'updated')
     search_fields = ('title', 'description', 'user__username')
     ordering = ('title', 'completed')
+    actions = ('mark_as_completed', 'mark_as_uncompleted')
 
 
+    @admin.action(description='mark as completed')
+    def mark_as_completed(self, request, queryset):
+        queryset.update(completed=True)
 
-admin.site.register(Task, TaskAdmin)
+
+    @admin.action(description='mark as uncompleted')
+    def mark_as_uncompleted(self, request, queryset):
+        queryset.update(completed=False)
