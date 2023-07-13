df07977f36e5562d1ecc1ad8444853440cce5a04
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-13 17:40:09 +0500

    ordering

--- api/admin.py
+++ api/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'completed')
     list_filter = ('completed', 'created', 'updated')
     search_fields = ('title', 'description', 'user__username')
+    ordering = ('title', 'completed')
 
 
 
