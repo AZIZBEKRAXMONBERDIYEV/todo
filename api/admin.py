13438fd0f0472496cd3d6f5332e847bca17a61e9
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-13 17:33:34 +0500

    search fields

--- api/admin.py
+++ api/admin.py
@@ -5,6 +5,7 @@ from .models import Task
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title',)
     list_filter = ('completed', 'created', 'updated')
+    search_fields = ('title', 'description', 'user__username')
 
 
 
