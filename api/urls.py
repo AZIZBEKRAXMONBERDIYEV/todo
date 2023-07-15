fb1a21350ecb5e2cb0733688c63bdaac5a56b3a1
djumanov <diyorbekjumanov1120@gmail.com>
2023-07-15 16:33:50 +0500

    generate token as api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, Login
+# from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('tasks/', TaskList.as_view()),
     path('tasks/<int:pk>/', TaskDetail.as_view()),
+    path('login/', Login.as_view()),
+    # path('login/', obtain_auth_token),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from django.contrib.auth.models import User
 from .models import Task
@@ -12,6 +14,7 @@ from .serializers import TaskSerializer, UserSerializer
 
 
 class TaskList(APIView):
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request: Request) -> Response:
@@ -64,3 +67,12 @@ class TaskDetail(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class Login(ObtainAuthToken):
+    def post(self, request: Request) -> Response:
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user: User = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response(token.key)
